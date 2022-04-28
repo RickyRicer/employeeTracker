e0f83804acd6ac9f24aa398029a0a7651dea4f51
Rick Ansay <rick.ansay@gmail.com>
2022-04-27 21:30:14 -0600

    Create greeting and start db

--- server.js
+++ server.js
@@ -2,3 +2,26 @@ const connection = require('./config');
 const inquirer = require('inquirer');
 require('console.table');
 
+const options = [
+    'View All Departments',
+    'View All Roles',
+    'View All Employees',
+    'Add A Department',
+    'Add A Role',
+    'Add An Employee',
+    'Update An Employee Role',
+    'Exit',
+]
+
+const greeting = () => {
+    console.log(`
+    ***********************************
+    *                                 *
+    *        Employee Tracker         *
+    *                                 *
+    ***********************************
+    `)
+}
+
+greeting();
+
