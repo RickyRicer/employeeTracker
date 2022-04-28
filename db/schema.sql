8ded66614cff6f3a2fefc7738877ef87440bd3cd
Rick Ansay <rick.ansay@gmail.com>
2022-04-27 21:39:42 -0600

    Create schema

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS employee_db; 
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
