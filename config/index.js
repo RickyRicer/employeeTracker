0d637d11bf1992682b7b7dc365a5ec8f3ba5496c
Rick Ansay <rick.ansay@gmail.com>
2022-04-28 19:35:07 -0600

    Create inquirer logic

--- config/index.js
+++ config/index.js
@@ -5,9 +5,10 @@ const connection = mysql
   .createConnection({
     host: "localhost",
     user: process.env.DB_USER,
-    password: process.env.DB_PW,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   })
   .promise();
 
 module.exports = connection;
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const connection = require('./config');
 const inquirer = require('inquirer');
 require('console.table');
 
-const options = [
+const choices = [
     'View All Departments',
     'View All Roles',
     'View All Employees',
@@ -15,13 +15,71 @@ const options = [
 
 const greeting = () => {
     console.log(`
-    ***********************************
-    *                                 *
-    *        Employee Tracker         *
-    *                                 *
-    ***********************************
+                                                                                                          
+    ###### #    # #####  #       ####  #   # ###### ######    ##### #####    ##    ####  #    # ###### #####  
+    #      ##  ## #    # #      #    #  # #  #      #           #   #    #  #  #  #    # #   #  #      #    # 
+    #####  # ## # #    # #      #    #   #   #####  #####       #   #    # #    # #      ####   #####  #    # 
+    #      #    # #####  #      #    #   #   #      #           #   #####  ###### #      #  #   #      #####  
+    #      #    # #      #      #    #   #   #      #           #   #   #  #    # #    # #   #  #      #   #  
+    ###### #    # #      ######  ####    #   ###### ######      #   #    # #    #  ####  #    # ###### #    #                                                                                   
     `)
 }
 
 greeting();
 
+const init = async () => {
+    await inquirer
+     .prompt([{
+         type: 'list',
+         message: 'Which option would you like to select?',
+         name: 'choice',
+         choices: choices,
+         default() {
+             return 'View All Departments';
+         },
+     }]).then((response) => {
+         let userChoice = response.option;
+         switch (userChoice){
+             //User exits
+            case choices[7]:
+                console.log('Exiting tracker...');
+                connection.end();
+                process.exit();
+            case choices[0]:
+                viewAllDepartments();
+                break;
+            case choices[1]: // View All Roles
+                viewAllRoles();
+                break;
+            case choices[2]: // View All Employees
+                viewAllEmployees();
+                break;
+            case choices[3]: // Add A Department
+                addDepartment();
+                break;
+            case choices[4]: // Add A Role
+                addRole();
+                break;
+            case choices[5]: // Add An Employee
+                addEmployee();
+                break;
+            case choices[6]: // Update An Employee Role
+                updateEmployeeRole();
+                break;
+            default: // Exit
+                console.log('Exiting default...');
+                connection.end(); 
+                process.exit(); 
+            } 
+         })
+         .catch((error) => {
+             if (error.isTtyError) {
+                 console.log(`Error: Prompt couldn't be rendered`);
+             } else {
+                 console.log(`Error: ${error}`);
+                 process.exit();
+             }
+         })
+}
+
+init();
