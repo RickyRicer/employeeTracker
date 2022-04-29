91f0fd025c3d41f4ea4a0ee704a19109f5e8f8cb
Rick Ansay <rick.ansay@gmail.com>
2022-04-28 22:45:49 -0600

    Add all employee logic

--- server.js
+++ server.js
@@ -118,6 +118,30 @@ const viewAllRoles = async () => {
     }
   };
 
-  
+  const viewAllEmployees = async () => {
+    try {
+      const employeeSelecter = `SELECT e.id,
+                                e.first_name, 
+                                e.last_name, 
+                                r.title AS 'job title',
+                                d.name AS department,
+                                r.salary AS salary,
+                                CONCAT (m.first_name, " ", m.last_name) AS manager
+                              FROM employee e
+                              LEFT JOIN role r ON e.role_id = r.id 
+                              LEFT JOIN department d ON r.department_id = d.id
+                              LEFT JOIN employee m ON e.manager_id = m.id;`;
+      const [employees] = await connection.query(employeeSelecter);
+      console.log(`
+      
+      #### All Employees ####
+      `);
+      console.table(employees);
+      init();
+    } catch (e) {
+      console.log(`Error: ${e}`);
+      process.exit(); 
+    }
+  }; 
 
 init();
